4ad8f82e463fd5f138b00823fbf518fb8a26f2c5
ASOKKUMAR <asokgithub@gmail.com>
2026-04-11 13:46:17 +0530

    fix cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,13 @@ const app = express();
 connectDatabase();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
