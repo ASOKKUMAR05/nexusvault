e60236846298fd7b4f674167bf432fcde28a05c4
ASOKKUMAR <asokgithub@gmail.com>
2026-04-18 20:37:42 +0530

    fix: cloudfront

--- backend/src/utils/cloudfront.js
+++ backend/src/utils/cloudfront.js
@@ -1,14 +1,14 @@
 const { getSignedUrl } = require("@aws-sdk/cloudfront-signer");
 
-export const getCloudFrontSignedUrl = (fileKey) => {
+const getCloudFrontSignedUrl = (fileKey) => {
     const url = `https://${process.env.CLOUDFRONT_DOMAIN}/${fileKey}`;
 
-    const signedUrl = getSignedUrl({
+    return getSignedUrl({
         url,
         keyPairId: process.env.CLOUDFRONT_KEY_PAIR_ID,
-        privateKey: process.env.CLOUDFRONT_PRIVATE_KEY,
-        dateLessThan: new Date(Date.now() + 1000 * 60 * 60) // 1 hour
+        privateKey: process.env.CLOUDFRONT_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        dateLessThan: new Date(Date.now() + 1000 * 60 * 60)
     });
-
-    return signedUrl;
 };
+
+module.exports = { getCloudFrontSignedUrl };
